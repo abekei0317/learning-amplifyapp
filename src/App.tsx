e016a2333b4673a9aeb81d0d96af18acf1c3a05d
abekei0317 <abekei0317@gmail.com>
2021-08-08 21:13:05 +0900

    add image storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import { API, graphqlOperation } from 'aws-amplify'
+import { API, Storage, graphqlOperation } from 'aws-amplify'
 import { GraphQLResult } from '@aws-amplify/api'
 import * as queries from './graphql/queries'
 import * as mutations from './graphql/mutations'
@@ -13,8 +13,12 @@ import {
   DeleteNoteMutationVariables,
 } from './API'
 
+const excludeNullsFromArray = (arr: Array<any>): Array<any> => {
+  return arr.filter((item): item is NonNullable<typeof item> => item != null)
+}
+
 const App = () => {
-  const initialFormState = { name: '', description: '' }
+  const initialFormState = { name: '', description: '', image: '' }
   const [notes, setNotes] = useState<Note[]>([])
   const [formData, setFormData] = useState(initialFormState)
 
@@ -26,12 +30,18 @@ const App = () => {
     const responce = (await API.graphql(
       graphqlOperation(queries.listNotes)
     )) as GraphQLResult<ListNotesQuery>
-    if (responce.data?.listNotes?.items) {
-      const notes = responce.data?.listNotes?.items.filter(
-        (item): item is NonNullable<typeof item> => item != null
-      )
-      setNotes(notes)
-    }
+    const notesFromAPI = responce.data?.listNotes?.items
+    if (!notesFromAPI) return
+    const notes = await Promise.all(
+      excludeNullsFromArray(notesFromAPI).map(async (note) => {
+        if (note.image) {
+          const image = await Storage.get(note.image)
+          note.image = image
+        }
+        return note
+      })
+    )
+    setNotes(notes)
   }
 
   const createNote = async () => {
@@ -41,9 +51,14 @@ const App = () => {
         input: formData,
       } as CreateNoteMutationVariables)
     )) as GraphQLResult<CreateNoteMutation>
-    if (responce.data?.createNote) {
-      setNotes([...notes, responce.data?.createNote])
+    const noteFromAPI = responce.data?.createNote
+    if (!noteFromAPI) return
+    if (formData.image) {
+      const image = await Storage.get(formData.image)
+      if (typeof image === 'object') return //NOTE: Objectの場合未考慮
+      formData.image = image
     }
+    setNotes([...notes, noteFromAPI])
     setFormData(initialFormState)
   }
 
@@ -56,6 +71,14 @@ const App = () => {
         input: { id },
       } as DeleteNoteMutationVariables)
     )
+  }
+
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files === null || !e.target.files[0]) return
+    const file = e.target.files[0]
+    setFormData({ ...formData, image: file.name })
+    await Storage.put(file.name, file)
+    fetchNotes()
   }
 
   return (
@@ -74,6 +97,7 @@ const App = () => {
           placeholder="Note description"
           value={formData.description}
         />
+        <input type="file" onChange={onChange} />
         <button onClick={createNote}>Create Note</button>
         <div style={{ marginBottom: 30 }}>
           {notes.map((note) => (
@@ -81,6 +105,9 @@ const App = () => {
               <h2>{note.name}</h2>
               <p>{note.description}</p>
               <button onClick={() => deleteNote(note)}>Delete note</button>
+              {note.image && (
+                <img src={note.image} alt="note" style={{ width: 400 }} />
+              )}
             </div>
           ))}
         </div>
